693993cf9403b77316c93ec69a90c3bcc4c24489
oumaima-chachi <chachioumaima4@gmail.com>
2025-01-27 13:16:20 +0100

    1000

---
+++ Level_03/rstr_capitalizer/rstr_capitalizer.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+
+
+void	rstr_capitalizer(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	i = 0;
+
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || str[i
+				+ 1] == '\t' || str[i + 1] == '\0'))
+		{
+			str[i] -= 32;
+		}
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int i;
+
+	if (argc < 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		rstr_capitalizer(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
